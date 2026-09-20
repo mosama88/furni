6e03045171cd2bf33fb581d24331d84a63d3e939
Mohamed Osama <mosama88@hotmail.com>
2026-09-20 10:14:53 +0300

    Install Packages2

--- routes/admin.php
+++ routes/admin.php
@@ -1,12 +1,19 @@
 <?php
 
+use App\Http\Controllers\Dashboard\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/admin', function () {
-    dd('Admin Dashboard');
-})->name('admin.dashboard');
 
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth:admin', 'verified'])->name('dashboard');
+
+Route::middleware(['auth:admin', 'verified'])->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('index');
+});
+
+
+Route::get('/ok', function () {
+    dd('OK OK');
+});
+
+
+require __DIR__ . '/admin-auth.php';
